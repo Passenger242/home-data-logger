9327b630ebb3e1fc320d86e770583b5463c50f03
Magnus Forslund <magnus.forslund@permobil.com>
2019-01-25 13:51:41 +0100

    query params on get temperatures

--- controller/temperatureController.js
+++ controller/temperatureController.js
@@ -4,17 +4,38 @@ const TemperatureModel = require('../model/temperatureModel');
 const mongoose = require('mongoose');
 
 exports.findAllTemperatures = function (req, res) {
-    TemperatureModel.find({}, function (err, result) {
-        if (err)  {
-          res.send(err);
-        }
-        res.json(result);
-    });
+  const sensorParam = req.query['sensor'];
+  const fromParam = req.query['from'];
+  const tomParam = req.query['tom']
+
+  var query = {};
+
+  if(sensorParam) {
+    query.sensor = sensorParam;
+  }
+
+  if(fromParam || tomParam) {
+    query.ts = {};
+    if(fromParam) {
+      query.ts.$gte = Date.parse(fromParam);
+    }
+    if(tomParam) {
+      query.ts.$lte = Date.parse(tomParam);
+    }
+  }
+
+  TemperatureModel.find(query, function (err, result) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(result);
+  })
+    .sort({ ts: -1 });
 };
 
 exports.createTemperature = function (req, res) {
-    var newTemperature = new TemperatureModel(req.body);
-    newTemperature.save(function(err, temperature) {
+  var newTemperature = new TemperatureModel(req.body);
+  newTemperature.save(function (err, temperature) {
     if (err) {
       res.send(err);
     }
